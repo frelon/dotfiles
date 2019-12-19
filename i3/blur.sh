b35cb29c0edc33fc3347a066438974b74ba04436
Fredrik Lönnegren <fredrik.lonnegren@iver.se>
2019-12-19 12:40:11 +0100

    I3 again

---
+++ i3/blur.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+icon="$HOME/Pictures/icon.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 20% -scale 500% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -u -i "$tmpbg"
+rm "$tmpbg"
