46697c0f98df960e6df307ab50780410295d5908
Fredrik Lönnegren <fredrik.lonnegren@gmail.com>
2020-02-17 21:19:56 +0100

    Add ssh-agent and gpg-agent

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,8 @@ set -x EDITOR 'nvim'
 set -x GOPATH '/home/frelon/go'
 set -x PATH $PATH $GOPATH/bin
 set -x GO111MODULE on
+set -x GPG_TTY (tty)
+
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+echo "UPDATESTARTUPTTY" | gpg-connect-agent > /dev/null 2>&1
+
