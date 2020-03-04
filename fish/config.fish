bbb63abad3373f38a7b8ba204a8e166a177b1699
Fredrik Lönnegren <fredrik.lonnegren@internetstiftelsen.se>
2020-03-04 11:01:40 +0100

    PATH

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 set -x EDITOR 'nvim'
 set -x GOPATH '/home/frelon/go'
-set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH $GOPATH/bin '/home/frelon/.local/bin'
 set -x GO111MODULE on
 set -x GPG_TTY (tty)
 set -x DOCKERFILES_REPO_PATH '/home/frelon/src/dockerfiles'
