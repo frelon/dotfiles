a9a5d4d8a3963490b0a08b27ba5778eb2936d1ec
Fredrik Lönnegren <fredrik.lonnegren@internetstiftelsen.se>
2020-09-07 13:23:49 +0200

    add environment variable for DOCKERFILES_DIR and alias for git-delete-merged

--- fish/config.fish
+++ fish/config.fish
@@ -4,9 +4,10 @@ set -x PATH $PATH $GOPATH/bin '/home/frelon/.local/bin'
 set -x GO111MODULE on
 set -x GPG_TTY (tty)
 set -x DOCKERFILES_REPO_PATH '/home/frelon/src/dockerfiles'
+set -x DOCKERFILES_DIR '/home/frelon/src/dockerfiles'
 set -x DOTFILES_REPO_PATH '/home/frelon/src/dotfiles'
 set -x NVIM_CONFIG '/home/frelon/.config/nvim/init.vim'
-
 set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 echo "UPDATESTARTUPTTY" | gpg-connect-agent > /dev/null 2>&1
 
+alias git-delete-merged "git branch --merged | egrep -v \"(^\*|master|develop|stage)\" | xargs git branch -d"
