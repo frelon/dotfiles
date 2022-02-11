c3a2e595ad6e974f9fe5bd1a98273370fa7f1fb0
Fredrik Lönnegren <fredrik.lonnegren@internetstiftelsen.se>
2022-02-11 14:48:53 +0100

    Change locksreen script

--- i3/scrlock.sh
+++ i3/scrlock.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
-# Glichting screen lock script by xzvf
-logger -p CRIT "Glitching screen"
 
-pngfile="/tmp/sclock.png"
-bmpfile="/tmp/sclock.bmp"
-glitchedfile="/tmp/sclock_g.bmp"
+FILE=/tmp/norrsken.jpg
+FINAL=/tmp/norrsken.png
+URL=https://images.hdqwalls.com/download/aurora-northern-lights-4k-hh-2560x1440.jpg
+SIZE=2560x1440
 
-scrot -z $pngfile
+if [ ! -f "$FILE" ]; then
+    wget "$URL" -O "$FILE"
+fi
 
-# convert to bmp and pixelate
-magick convert -rotate -90 $pngfile $bmpfile
+SCREEN_RESOLUTION="$(xdpyinfo | grep dimensions | cut -d' ' -f7)"
+BGCOLOR="000000"
+convert "$FILE" -gravity Center -background $BGCOLOR -extent "$SCREEN_RESOLUTION" RGB:- | i3lock --raw "$SCREEN_RESOLUTION":rgb -c $BGCOLOR -i /dev/stdin
 
-for a in {1,2,4,5,10}
-do
-    # Glitch it with sox FROM: https://maryknize.com/blog/glitch_art_with_sox_imagemagick_and_vim/
-    sox -t ul -c 1 -r 48k $bmpfile -t ul $glitchedfile trim 0 90s : echo 1 1 $((a*2)) 0.1
-    # Rotate it by 90 degrees
-    magick convert -scale $((100/(a)))% -scale $((100*(a)))% -rotate 90 $glitchedfile $bmpfile
-done
-
-# Add lock icon, pixelate and convert back to png
-magick convert -gravity center -font "Hack-Bold-Nerd-Font-Complete-Mono" \
-    -pointsize 200 -draw "text 0,0 ''" -channel RGBA -fill '#bf616a' \
-    $bmpfile $pngfile
-
-i3lock -n -e -u -i $pngfile
-#feh $pngfile
-rm $pngfile $bmpfile $glitchedfile
